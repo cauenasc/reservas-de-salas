7e416264716b8f9d90dfe4ce4cbcaf071eda9578
cauenasc <cauenasc@outlook.com>
2025-10-13 10:59:33 -0300

    First commit

---
+++ reservas-de-salas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reservas_de_salas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
